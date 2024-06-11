fa557df0ef15f28e6b6aabed3e9c46ffdc89d541
Izzatul Masruroh <izza@zero-one-group.com>
2024-06-11 18:47:46 +0700

    feat/5-add-edit-profile-screen

---
+++ lib/src/features/profile/edit/edit_profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Edit Profile'),
+      ),
+    );
+  }
+}
--- lib/src/features/profile/profile_screen.dart
+++ lib/src/features/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slicing_ui/src/features/profile/edit/edit_profile_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,9 +11,23 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Profile'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Profile'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const EditProfileScreen()),
+                );
+              },
+              child: const Text('edit profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
